531b9aae5e69c7976ee8dbfb07d5e5dc1ee244de
georgethecode <87601989+georgethecode@users.noreply.github.com>
2021-07-18 19:51:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ app=Flask(__name__)
 
 # run_with_ngrok(app)
 
- @app.route('/')
- def hello():
-  return('Hello world!')
+@app.route('/')
+def hello():
+ return('Hello world!')
+
+if __name__ == "__main__":
+ app.run()
